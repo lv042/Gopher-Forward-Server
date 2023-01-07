7a562700bdc33fc1c85cff10869f9dc069b6aba6
Luca v. K <luca.v.kannen@gmail.com>
2023-01-07 22:02:10 +0100

    Add option to get hostnames of the server

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"io"
 	"log"
 	"net"
+	"strings"
+)
+
+var (
+	hosts map[string]net.Conn
 )
 
 func main() {
+	hosts = make(map[string]net.Conn)
+
 	// Listen for incoming connections
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
@@ -22,30 +30,78 @@ func main() {
 			continue
 		}
 
-		// Connect to the other server or device
-		otherConn, err := net.Dial("tcp", "other-server:8080")
-		if err != nil {
-			log.Println(err)
-			conn.Close()
-			continue
-		}
-
-		// Copy data between the two connections
-		go func() {
-			_, err := io.Copy(conn, otherConn)
-			if err != nil {
-				log.Println(err)
-			}
-			conn.Close()
-			otherConn.Close()
-		}()
-		go func() {
-			_, err := io.Copy(otherConn, conn)
-			if err != nil {
-				log.Println(err)
-			}
-			conn.Close()
-			otherConn.Close()
-		}()
+		// Handle the connection in a new goroutine
+		go handleConnection(conn)
 	}
 }
+
+func handleConnection(conn net.Conn) {
+	// Read the hostname from the client
+	hostname, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		log.Println(err)
+		conn.Close()
+		return
+	}
+	hostname = strings.TrimSpace(hostname)
+
+	// Add the host to the list of known hosts
+	hosts[hostname] = conn
+
+	// Create a buffer to hold incoming data
+	buf := make([]byte, 1024)
+
+	for {
+		// Read incoming data
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				log.Println(err)
+			}
+			delete(hosts, hostname)
+			conn.Close()
+			return
+		}
+
+		// Check for special commands
+		data := strings.TrimSpace(string(buf[:n]))
+		if data == "list hosts" {
+			// Send a list of known hosts
+			hostList := "Known hosts:\n"
+			for h := range hosts {
+				hostList += h + "\n"
+			}
+			conn.Write([]byte(hostList))
+		} else if strings.HasPrefix(data, "connect ") {
+			// Connect to another host
+			otherHost := strings.TrimPrefix(data, "connect ")
+			otherConn, ok := hosts[otherHost]
+			if !ok {
+				conn.Write([]byte("Error: Unknown host.\n"))
+				continue
+			}
+
+			// Copy data between the two connections
+			go func() {
+				_, err := io.Copy(conn, otherConn)
+				if err != nil {
+					log.Println(err)
+				}
+				conn.Close()
+				otherConn.Close()
+		
+				}()
+				go func() {
+					_, err := io.Copy(otherConn, conn)
+					if err != nil {
+						log.Println(err)
+					}
+					conn.Close()
+					otherConn.Close()
+				}()
+			} else {
+				// Send an error message
+				conn.Write([]byte("Error: Unknown command.\n"))
+			}
+		}
+	}
